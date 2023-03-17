d635035d11bb5ae8e42c56e6853eb3ac812f1c31
Claudia-Nicol Zaitseva <114983016+p4nch1ko@users.noreply.github.com>
2023-03-17 14:33:42 +0200

    kotrollt66

--- 05_kontrolltoo1/kontrolt66_harmooniline_keskmine/src/Main.java
+++ 05_kontrolltoo1/kontrolt66_harmooniline_keskmine/src/Main.java
@@ -1,6 +1,6 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.*;
 
 public class Main {
 
@@ -15,19 +15,36 @@ public class Main {
 //* Kilomeetriste lõikude läbimiste keskmised kiirused loetakse sisse failist, kogu teekonna läbimise keskmine
 //  kiirus salvestatakse faili.
 
-// kiirusedTekstiFail.txt  <-- teksti fail kus on kiirused
+//  kiirusedTekstiFail.txt  <-- teksti fail kus on kiirused
 
     public static void main(String[] args) {
         double kiirus1 = 65.0; // km/h
         double kiirus2 = 40.0; // km/h
         double keskmine1 = keskmineKiirus(kiirus1, kiirus2);
         System.out.println("Keskmine kiirus: " + keskmine1 + " km/h");
+
         // ------------------------------------------------------------------------------------
+
         double[] antudKiirused = { 30.0, 80.0, 65.0, 40.0, 75.0 }; // km/h
         double keskmine2 = keskmineKiirustest(antudKiirused);
         System.out.println("Keskmine kiirus antud kiirustest oli: " + keskmine2 + " km/h");
+
         // ------------------------------------------------------------------------------------
 
+        Scanner scanner = new Scanner(new File("kiirused.txt"));
+
+        double kiirusedSumma = 0;
+        int kmKokku = 0;
+        while (scanner.hasNextDouble()){
+            kiirusedSumma += scanner.nextDouble();
+            kmKokku++;
+        }
+
+        double keskmine3 = kiirusedSumma / kmKokku;
+
+        PrintWriter pw = new PrintWriter("keskminekiirus.txt");
+        pw.printf("%.2f", keskmine3);
+        pw.close();
     }
 
 
