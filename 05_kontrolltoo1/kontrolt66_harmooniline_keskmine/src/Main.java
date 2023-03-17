091d3c0c9acd5f0e12b66d5fe26f594b50184791
Claudia-Nicol Zaitseva <114983016+p4nch1ko@users.noreply.github.com>
2023-03-17 13:02:03 +0200

    kontrollt66

--- 05_kontrolltoo1/kontrolt66_harmooniline_keskmine/src/Main.java
+++ 05_kontrolltoo1/kontrolt66_harmooniline_keskmine/src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(keskmineKiirus(30, 60));
     }
 
-    
+    public static double keskmineKiirus(double kiirus1, double kiirus2) {
+        double keskmineKiirus = (2*kiirus1*kiirus2)/(kiirus1+kiirus2);
+        System.out.println("keskmine kiirus oli " + keskmineKiirus);
+        return keskmineKiirus;
+    }
+
 }
