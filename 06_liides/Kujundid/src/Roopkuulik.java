59dbb073c20e064696d87d4476691bb025e36b5c
Claudia-Nicol Zaitseva <114983016+p4nch1ko@users.noreply.github.com>
2023-05-03 18:14:06 +0300

    Kujundid automaatnetest

---
+++ 06_liides/Kujundid/src/Roopkuulik.java
@@ -0,0 +1,20 @@
+public class Roopkuulik extends Tasapinnaline implements Arvutused{
+    double kylg1;
+    double kylg2;
+    double kylg3;
+    double kulg4;
+
+    public Roopkuulik(int tippudeArv) {
+        super(tippudeArv);
+    }
+
+    @Override
+    public double arvutaYmberMoot() {
+        return 0;
+    }
+
+    @Override
+    public double arvutaPindala() {
+        return 0;
+    }
+}
